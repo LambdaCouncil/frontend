3ce12e180c29a7064ae222aa5d5bb53bb6a49416
Greg Johnston <GCJohnston91@gmail.com>
2019-10-23 17:21:40 -0400

    Rate Councils styled with star icons

--- components/Settings/RateCouncils.js
+++ components/Settings/RateCouncils.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import {KeyboardAvoidingView, StyleSheet, View} from "react-native";
-import {H1} from 'native-base'
+import {Text, KeyboardAvoidingView, StyleSheet, View} from "react-native";
+import {H1, H3} from 'native-base'
 import {Link, withRouter} from 'react-router-native'
 import Icon from "../Icon";
+import {AirbnbRating, Rating} from "react-native-ratings";
+
 
 const RateCouncils = (props) => {
+
+  const _valueChanged = (rating) => {
+    console.log(rating)
+  };
+
   return (
     <KeyboardAvoidingView
       style={styles.inputContainer}
@@ -17,8 +24,26 @@ const RateCouncils = (props) => {
           style={styles.backButton}
         />
       </Link>
-
-      <H1>Rate Councils</H1>
+      <View style={styles.pageView}>
+        <Text style={styles.header}>How Would You Rate Our App?</Text>
+        <AirbnbRating
+          count={5}
+          defaultRating={5}
+          size={50}
+          selectedColor='#288365'
+          showRating={false}
+        />
+        <View>
+          <Text style={styles.reviewText}>
+            Would you mind to share your feedback on the App Store?
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.appStoreLink}>
+            Go to App Store
+          </Text>
+        </View>
+      </View>
 
     </KeyboardAvoidingView>
   );
@@ -36,11 +61,39 @@ const styles = StyleSheet.create({
     top: 25,
     left: 5,
     width: '100%',
-    height: 50
+    height: 50,
+  },
+  pageView: {
+    marginHorizontal: 20
   },
   backButton: {
     fontSize: 50
+  },
+  appStoreLink: {
+    color: '#288365',
+    marginTop: 20,
+    fontSize: 17,
+    fontFamily: 'bern2',
+    fontWeight: '600',
+    textAlign: 'center'
+  },
+  reviewText: {
+    color: 'black',
+    marginTop: 32,
+    fontSize: 17,
+    marginVertical: 20,
+    textAlign: 'center',
+    fontFamily: 'bern2'
+  },
+  header: {
+    fontSize: 28,
+    textAlign: 'center',
+    marginBottom: 20,
+    fontFamily: 'gotham',
+    fontWeight: '500',
+    width: 335,
+    marginLeft: 20
   }
-})
+});
 
 export default RateCouncils;
