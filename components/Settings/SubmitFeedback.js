5c046bab03f0252f8dd6eabdc156600aa886b4ee
Greg Johnston <GCJohnston91@gmail.com>
2019-10-24 14:44:21 -0400

    Submit Feedback styled 90%.

--- components/Settings/SubmitFeedback.js
+++ components/Settings/SubmitFeedback.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import firebase from "../../firebase"
-import { KeyboardAvoidingView, StyleSheet, TextInput, View } from 'react-native'
-import { Input, Text, Label, Item, H1, H3 } from 'native-base'
-import { Link, withRouter } from 'react-router-native'
-import { connect } from 'react-redux'
+import {KeyboardAvoidingView, StyleSheet, TextInput, View} from 'react-native'
+import {Input, Text, Label, Item, H1, H3} from 'native-base'
+import {Link, withRouter} from 'react-router-native'
+import {connect} from 'react-redux'
 
 import Icon from '../Icon'
-import { signUpDisplayName } from '../../actions'
+import {signUpDisplayName} from '../../actions'
 
 function SubmitFeedback(props) {
 
@@ -30,21 +30,22 @@ function SubmitFeedback(props) {
           style={styles.backButton}
         />
       </Link>
-
-      <H1>Submit Feedback</H1>
-
-      <H3>What Can We Improve?</H3>
-      <View>
-        <TextInput
-          // style={styles.textInput}
-          editable
-          maxLength={500}
-        />
+      <View style={styles.pageView}>
+        <Text style={styles.textContent}>Submit Feedback</Text>
+        <Text style={styles.textContent}>What Can We Improve?</Text>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            multiline={true}
+            editable
+            maxLength={500}
+            placeholder='Please type your feedback here...'
+          />
+        </View>
+        <Link to='/settings'>
+          <Text style={styles.submit}>Submit</Text>
+        </Link>
       </View>
-
-      <Link to='/settings'>
-        <H3>Submit</H3>
-      </Link>
     </KeyboardAvoidingView>
 
   )
@@ -54,9 +55,9 @@ function SubmitFeedback(props) {
 const styles = StyleSheet.create({
   inputContainer: {
     height: '100%',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    // flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center'
   },
   link: {
     position: 'absolute',
@@ -72,7 +73,24 @@ const styles = StyleSheet.create({
     marginVertical: 10
   },
   textInput: {
-    textAlign: 'left'
+    fontSize: 17
+  },
+  pageView: {
+    marginHorizontal: 20,
+    marginTop: 80,
+  },
+  textContent: {
+    fontSize: 28,
+    marginBottom: 10,
+    fontFamily: 'gotham',
+    fontWeight: '500',
+  },
+  submit: {
+    // marginVertical: 15,
+    color: '#288365',
+    textAlign: 'center',
+    fontSize: 17,
+    top: 169
   }
 })
 
