23336b11f7901f855c0c2eb82025056eb9e1cb7f
Anna <townsendanna15@gmail.com>
2019-10-24 18:22:11 -0400

    initial commit for donations

---
+++ components/Donations/Donations.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { withRouter } from 'react-router-native'
+
+
+const Donations = props => {
+    return (
+        <div>
+            <p>Join Our Monthly Giving Program</p>
+        <p>100% of your donation supports development of this platform so members can more effectively magnify their callings.</p>
+        </div>
+    )
+}
+
+export default withRouter(Donations)
