69fc797768be132488084068142f1145d0dcc4bb
Anna <townsendanna15@gmail.com>
2019-10-29 16:21:22 -0400

    added public api key and started component working on form now

---
+++ components/Donations/DonationAmount.js
@@ -0,0 +1,20 @@
+import React from "react";
+import firebase from "../../firebase";
+import {KeyboardAvoidingView, StyleSheet, View, Text, Button} from 'react-native';
+import { withRouter } from 'react-router-native';
+import {StripeProvider, Elements} from 'react-stripe-elements';
+
+
+export default class App extends React.Component<IAppProps, IAppState> {
+    render(){
+        return (
+            <StripeProvider apiKey="pk_test_6zLTl66mCBKJihsb5xN6i6qZ">
+                <Elements>
+
+
+                </Elements>
+            </StripeProvider>
+        )
+
+    }
+}
