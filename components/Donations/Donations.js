3ef3b737a3dc7f549e7b1a6fcc7688d7b5154a85
Anna <townsendanna15@gmail.com>
2019-10-29 08:22:17 -0400

    fixed button

--- components/Donations/Donations.js
+++ components/Donations/Donations.js
@@ -1,6 +1,6 @@
 import React from "react"
 import firebase from "../../firebase"
-import {KeyboardAvoidingView, StyleSheet, View, Text} from 'react-native'
+import {KeyboardAvoidingView, StyleSheet, View, Text, Button} from 'react-native'
 import { withRouter } from 'react-router-native'
 
 
@@ -9,7 +9,7 @@ const Donations = props => {
         <View>
             <Text>Join Our Monthly Giving Program</Text>
             <Text>100% of your donation supports development of this platform so members can more effectively magnify their callings.</Text>
-        <Button transparent>Send Donation</Button>
+        <Button transparent title="Send Donation">Send Donation</Button>
         </View>
     )
 }
