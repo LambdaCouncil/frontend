7f99dd9509309f4c65258301dddf68d318e73c77
jcrn <clemons.jason@gmail.com>
2019-10-30 22:56:37 -0700

    side panel initial styling complete, font sizes, colors, and dimensions same as provided in zeplin documentation. Requires further styling for active links

--- components/SidePanel/SideMenu.js
+++ components/SidePanel/SideMenu.js
@@ -12,52 +12,52 @@ const SideMenu = props => {
     
     <View style={{marginLeft: 20}}>
       <UserPanel />
-      <Button style={{marginTop: 26, height: '8%'}}>
+      <Button style={{marginTop: 26, height: '8%', elevation: 0}}>
         <Link to="/agendas" onPress={() => props.togglePanel()}>
           <Text style={{color: '#202224', fontSize: 17}}>Agendas</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/discussions" onPress={() => props.togglePanel()}>
           <Text style={{color: '#202224', fontSize: 17}}>Discussions</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/assignments" onPress={() => props.togglePanel()}>
           <Text style={{color: '#202224', fontSize: 17}}>Assignments</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/files" onPress={() => props.togglePanel()}>
           <Text style={{color: '#202224', fontSize: 17}}>Files</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/promptings" onPress={() => props.togglePanel()}>
           <Text style={{color: '#202224', fontSize: 17}}>Promptings</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/push-notifications" onPress={() => props.togglePanel()}>
           <Text style={{color: '#202224', fontSize: 17}}>Notifications</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/settings" onPress={() => props.togglePanel()}>
           <Text style={{color: '#6f777e', fontSize: 17}}>Settings</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/admin" onPress={() => props.togglePanel()}>
           <Text style={{color: '#6f777e', fontSize: 17}}>Admin</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/donations" onPress={() => props.togglePanel()}>
           <Text style={{color: '#6f777e', fontSize: 17}}>Donations</Text>
         </Link>
       </Button>
-      <Button style={{height: '8%'}}>
+      <Button style={{height: '8%', elevation: 0}}>
         <Link to="/login" onPress={() => props.togglePanel()}>
           <Text style={{color: '#6f777e', fontSize: 17}} onPress={() => firebase.auth().signOut()}>Log Out</Text>
         </Link>
