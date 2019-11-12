31e5479ede7b5cb486330a3a94168c229b740541
jcrn <clemons.jason@gmail.com>
2019-11-12 08:44:25 -0800

    added new firebase config

--- firebase.js
+++ firebase.js
@@ -3,25 +3,37 @@ import "firebase/auth"
 import "firebase/storage"
 import 'firebase/firestore'
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCPVmMRkhRB5ebmcl8L_60kmcWI-_KI1Ow",
-    authDomain: "council-c200f.firebaseapp.com",
-    databaseURL: "https://council-c200f.firebaseio.com",
-    projectId: "council-c200f",
-    storageBucket: "council-c200f.appspot.com",
+const firebaseConfig = process.env.NODE_ENV === 'production' ? {
+    apiKey: "AIzaSyA7vboizvDrRORHvkT01Z8vKCOuH8In4UA",
+    authDomain: "councils-589e4.firebaseapp.com",
+    databaseURL: "https://councils-589e4.firebaseio.com",
+    projectId: "councils-589e4",
+    storageBucket: "councils-589e4.appspot.com",
     messagingSenderId: "419030896972",
-    appId: "1:419030896972:android:0f56b665454136744dbfe9",
+    appId: "1:229687566803:android:9154556522d3253c",
     measurementId: "G-XTQP3M861X"
-}
+} : {
+        apiKey: "AIzaSyCPVmMRkhRB5ebmcl8L_60kmcWI-_KI1Ow",
+        authDomain: "council-c200f.firebaseapp.com",
+        databaseURL: "https://council-c200f.firebaseio.com",
+        projectId: "council-c200f",
+        storageBucket: "council-c200f.appspot.com",
+        messagingSenderId: "419030896972",
+        appId: "1:419030896972:android:0f56b665454136744dbfe9",
+        measurementId: "G-XTQP3M861X"
+    }
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
 /* 
     Import db into any component to shorten firebase syntax in function calls. 
-        example: firebase.firestore().collection(users).doc(uid) becomes: db.doc(uid)
+        example: firebase.firestore().collection(users).doc(uid) becomes: db(users).doc(uid)
 */
+
 export const db = table => firebase.firestore().collection(table)
 
 export default firebase
+
+
+
